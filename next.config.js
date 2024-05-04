ac2c7e04f1d2475d9dd69f6516a00ebbf40fa4d6
Marshal Nadar <marshalnadarmn18@gmail.com>
2024-05-04 13:00:20 +0530

    fixed route config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
-  },
   trailingSlash: true,
+  output: 'export',
 };
 
 module.exports = nextConfig;
